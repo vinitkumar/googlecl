a91286ae1ef73e61748626eac902a884e8a15d9b
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-12-14 23:44:49 +0530

    add clients for different service

--- src/googlecl/authenticate.py
+++ src/googlecl/authenticate.py
@@ -4,9 +4,12 @@ import threading
 import time
 from gdata.photos.service import PhotosService
 from gdata.docs.service import DocsService
-import gdata.media
+from gdata.blogger.service import BloggerService
+from gdata.contacts.service import  ContactsService
+from gdata.calendar.service import CalendarService
+from gdata.finance.service import FinanceService
+from gdata.youtube.service import YouTubeService
 from datetime import timedelta, datetime
-import gdata.geo
 from oauth2client import client
 
 
@@ -42,9 +45,33 @@ class Authenticate(object):
         expires_seconds = (expires-now).seconds
         # print ("Expires %s from %s = %s" % (expires,now,expires_seconds) )
         if self.service == 'docs':
-            gd_client = DocsService(email='default', additional_headers={'Authorization': 'Bearer %s' % credentials.access_token})
+            gd_client = DocsService(email='default', additional_headers={
+                'Authorization': 'Bearer %s' % credentials.access_token
+            })
         elif self.service == 'picasa':
-            gd_client = PhotosService(email='default', additional_headers={'Authorization': 'Bearer %s' % credentials.access_token})
+            gd_client = PhotosService(email='default', additional_headers={
+                'Authorization': 'Bearer %s' % credentials.access_token
+            })
+        elif self.service == 'finance':
+            gd_client = FinanceService(email='default', additional_headers={
+                'Authorization': 'Bearer %s' % credentials.access_token
+            })
+        elif self.service == 'calendar':
+            gd_client = CalendarService(email='default', additional_headers={
+                'Authorization': 'Bearer %s' % credentials.access_token
+            })
+        elif self.service == 'contacts':
+            gd_client = ContactsService(email='default', additional_headers={
+                'Authorization': 'Bearer %s' % credentials.access_token
+            })
+        elif self.service == 'youtube':
+            gd_client = YouTubeService(email='default', additional_headers={
+                'Authorization': 'Bearer %s' % credentials.access_token
+            })
+        elif self.service == 'blogger':
+            gd_client = BloggerService(email='default', additional_headers={
+                'Authorization': 'Bearer %s' % credentials.access_token
+            })
 
         d = threading.Thread(name='refresh_creds', target=self.refresh_creds, args=(credentials,expires_seconds - 10) )
         d.setDaemon(True)
