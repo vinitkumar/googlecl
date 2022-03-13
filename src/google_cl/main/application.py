a27e39196346e27c3aba057c192aac64dfdb91ea
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-03-13 15:26:03 +0530

    feat: implement auth as gphoto sync

--- src/google_cl/main/application.py
+++ src/google_cl/main/application.py
@@ -1,17 +1,48 @@
 import time
 import logging
+from pathlib import Path
 from google_cl import exceptions
+from google_cl.main.auth import Authorize
 
 from typing import Sequence
 
 
-LOG = logging.getLogger(__name__)
+LOG = logging .getLogger(__name__)
+APP_NAME = "googlecl"
 
 class Application:
 
     def __init__(self) -> None:
         self.start_time = time.time()
         self.catastrophic_failure = False
+        root_folder = Path(args.root_folder).absolute()
+
+        compare_folder = None
+        if args.compare_folder:
+            compare_folder = Path(args.compare_folder).absolute()
+        app_dirs = AppDirs(APP_NAME)
+
+        self.data_store = LocalData(db_path, args.flush_index)
+
+        credentials_file = db_path / ".gphotos.token"
+        if args.secret:
+            secret_file = Path(args.secret)
+        else:
+            secret_file = Path(app_dirs.user_config_dir) / "client_secret.json"
+        if args.new_token and credentials_file.exists():
+            credentials_file.unlink()
+
+        scope = [
+            "https://www.googleapis.com/auth/photoslibrary.readonly",
+            "https://www.googleapis.com/auth/photoslibrary.sharing",
+        ]
+        photos_api_url = (
+            "https://photoslibrary.googleapis.com/$discovery" "/rest?version=v1"
+        )
+        self.auth = Authorize(
+            scope, credentials_file, secret_file, int(args.max_retries)
+        )
+        self.auth.authorize()
 
     def exit_code(self) -> int:
         if self.catastrophic_failure:
